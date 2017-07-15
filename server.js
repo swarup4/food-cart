966302a994fb1ae7dca2f86eb28d2f95472547be
swarup4 <swarup.saha004@hotmail.com>
2017-07-16 01:12:13 +0530

    Create Main Server File

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 let express = require("express");
+let bodyParser = require("body-parser");
+let path = require("path");
 
 let app = express();
+
+app.get("/", function(req, res, next){
+    res.sendFile("index.html");
+})
 
 app.listen(3000, function(){
     console.log("Running");
